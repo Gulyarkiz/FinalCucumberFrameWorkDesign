ec61b636d7f82d0e2880fd863f5d54398491210a
Gulyar A <Gulyarkiz@gmail.com>
2023-01-19 15:44:23 -0600

    Initial commit

---
+++ src/test/java/pages/CreateUserTest.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+public class CreateUserTest {
+	
+	private String baseURL;
+	
+	@Test
+	public void createUser() {
+		// some code here
+	}
+	
+	@Test
+	public void updateUser() {
+		// some code here
+	}
+
+}
